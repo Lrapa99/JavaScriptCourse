955279dec30c7aa05fcf8c7261c6f17a48934d03
Lrapa99 <luisrapalino88@gmail.com>
2022-09-04 08:41:59 -0500

    Mis Notas Update

--- ejercicios/ejercicios.js
+++ ejercicios/ejercicios.js
@@ -1,7 +1,7 @@
 import { cadena } from "./1parte.js"; // importamos la constante cadena del modulo 1parte
 
 //*ejercicio 1:
-
+console.log("Ejercicio #1");
 function contarCaracteresCadena(valor) {
   console.log(`${cadena} y mi longitud es de ${cadena.length} caracteres`);
 }
@@ -9,7 +9,7 @@ function contarCaracteresCadena(valor) {
 contarCaracteresCadena(cadena);
 
 //*ejercicio 2:
-
+console.log("Ejercicio #2");
 function recortarCadena(valor) {
   const cadenaRecortada = valor.slice(7); //extraemos los datos que deseamos de la cadena completa y guardamos su valor en una nueva variable
 
@@ -19,7 +19,7 @@ function recortarCadena(valor) {
 recortarCadena(cadena);
 
 //*ejercicio 3:
-
+console.log("Ejercicio #3");
 function arrayCadena(valor) {
   const cadenaArreglo = valor.split(" ");
 
@@ -29,7 +29,7 @@ function arrayCadena(valor) {
 arrayCadena(cadena);
 
 //*ejercicio 4:
-
+console.log("Ejercicio #4");
 function repetirCadena(valor = null) {
   if (valor == null || valor == "") {
     console.log("Por favor ingrese un valor");
